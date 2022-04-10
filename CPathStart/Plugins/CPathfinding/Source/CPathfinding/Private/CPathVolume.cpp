9304947ca2c96452e3a8c0bf3b11006f79ffe196
DESKTOP-G4BFDQN\StrAnger <dtraut8@gmail.com>
2022-04-10 21:41:14 +0200

    Implemented example map 1, implemented first foxel generation with visualization

--- CPathStart/Plugins/CPathfinding/Source/CPathfinding/Private/CPathVolume.cpp
+++ CPathStart/Plugins/CPathfinding/Source/CPathfinding/Private/CPathVolume.cpp
@@ -20,8 +20,54 @@ void ACPathVolume::BeginPlay()
 {
 	Super::BeginPlay();
 	UBoxComponent* tempBox = Cast<UBoxComponent>(GetRootComponent());
-	DrawDebugBox(GetWorld(), GetActorLocation(), tempBox->GetScaledBoxExtent()*2,
+	DrawDebugBox(GetWorld(), GetActorLocation(), tempBox->GetScaledBoxExtent(),
 		FColor::Magenta, true, 10);
+
+	NodeCount[0] = VolumeBox->GetScaledBoxExtent().X*2 / VoxelSize + 1;
+	NodeCount[1] = VolumeBox->GetScaledBoxExtent().Y*2 / VoxelSize + 1;
+	NodeCount[2] = VolumeBox->GetScaledBoxExtent().Z*2 / VoxelSize + 1;
+	UE_LOG(LogTemp, Warning, TEXT("Count, %d %d %d"), NodeCount[0], NodeCount[1], NodeCount[2] );
+	GenerateGraph();
+	
+}
+
+void ACPathVolume::GenerateGraph()
+{
+	FVector Start = GetActorLocation() - VolumeBox->GetScaledBoxExtent();
+	int Index = 0;
+
+	//Reserving memory in array before adding elements
+	Nodes.Reserve(NodeCount[0]*NodeCount[1]*NodeCount[2]);
+	TraceHandles.Reserve(NodeCount[0]*NodeCount[1]*NodeCount[2]);
+	FCollisionShape TraceBox = FCollisionShape::MakeBox(FVector(VoxelSize/2));
+	
+	
+	for (int x = 0; x < NodeCount[0]; x++)
+	{
+		for (int y = 0; y < NodeCount[1]; y++)
+		{
+			for (int z = 0; z < NodeCount[2]; z++)
+			{
+				FVector Location = FVector(Start + FVector(x, y, z)*VoxelSize);
+				Nodes.Add(FCPathNode(Index, Location));
+				
+
+				TraceHandles.Add(GetWorld()->AsyncOverlapByChannel(
+					Location,
+					 FQuat(FRotator::ZeroRotator),
+					TraceChannel,
+					TraceBox,
+					FCollisionQueryParams::DefaultQueryParam,
+					FCollisionResponseParams::DefaultResponseParam,
+					nullptr, Index)
+				);
+				//if(DrawVoxels)
+				//	DrawDebugBox(GetWorld(), Location, TraceBox.GetExtent(), FColor::Green, true, 10);
+				Index++;
+			}
+		}
+	}
+
 	
 }
 
@@ -29,6 +75,32 @@ void ACPathVolume::BeginPlay()
 void ACPathVolume::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
+	if(TraceHandles.Num()>0)
+	{
+		for(int i = 0; i<TraceHandles.Num(); i++)
+		{
+			FOverlapDatum OverlapDatum;
+			if(GetWorld()->QueryOverlapData(TraceHandles[i], OverlapDatum))
+			{
+				TraceHandles.RemoveAtSwap(i);
+				
+				if(OverlapDatum.OutOverlaps.Num() > 0)
+				{
+					if(DrawVoxels)
+						DrawDebugBox(GetWorld(), OverlapDatum.Pos, FVector(VoxelSize/2), FColor::Red, true, 10);
+				}
+				else
+				{
+					if(DrawVoxels)
+						DrawDebugBox(GetWorld(), OverlapDatum.Pos, FVector(VoxelSize/2), FColor::Green, true, 10);
+				}
+				
+				i--;
+			}
+		}
+	}
+
 
 }
 
--- CPathStart/Plugins/CPathfinding/Source/CPathfinding/Public/CPathVolume.h
+++ CPathStart/Plugins/CPathfinding/Source/CPathfinding/Public/CPathVolume.h
@@ -4,7 +4,51 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "WorldCollision.h"
 #include "CPathVolume.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FCPathGroundData
+{
+	GENERATED_BODY()
+	FVector GroundSlope = FVector::ZeroVector;
+	FVector GroundAxis = FVector::ZeroVector;
+};
+
+USTRUCT(BlueprintType)
+struct FCPathNode
+{
+	GENERATED_BODY()
+public:
+
+	FCPathNode(){};
+	
+	FCPathNode(int Index, FVector Location)
+		:
+	WorldLocation(Location),
+	Index(Index)
+	{};
+	
+	
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	bool IsFree = true;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	bool IsGround = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FVector WorldLocation;
+	
+	int Index;
+	
+	TArray<FCPathNode*> FreeNeighbors;
+	
+	
+	
+};
+
 
 UCLASS()
 class ACPathVolume : public AActor
@@ -15,15 +59,47 @@ public:
 	// Sets default values for this actor's properties
 	ACPathVolume();
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = GenerationSettings, meta = (AllowPrivateAccess = "true"))
+	//Box to mark the area to generate graph in. It should not be rotated, the rotation will be ignored.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = GenerationSettings)
 	class UBoxComponent* VolumeBox;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = GenerationSettings)
+	TEnumAsByte<ECollisionChannel>  TraceChannel;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = GenerationSettings)
+	float AgentRadius = 20;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = GenerationSettings)
+	float AgentHeight = 150;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = GenerationSettings)
+	bool DrawVoxels = true;
+
+	//Size of the smallest voxel size, default: AgentRadius*2
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = GenerationSettings)
+	float VoxelSize = AgentRadius*2;
 	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	void GenerateGraph();
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	//Main array of nodes, used by pathfinding
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Graph)
+	TArray<FCPathNode> Nodes;
+
+private:
+	
+	
+	//Trace handles still waiting for execution
+	TArray<FTraceHandle> TraceHandles;
+	
+	//Dimension sizes of the Nodes array, XYZ 
+	int NodeCount[3];
 
 };
